fdad15b741301aabdc5cbab28e3168f658c2762a
jimcv <71361453+jimcv@users.noreply.github.com>
2022-03-18 18:23:36 -0400

    3.4 calling backend services

--- EventRegistration-Frontend/src/components/registration.js
+++ EventRegistration-Frontend/src/components/registration.js
@@ -1,3 +1,14 @@
+import axios from 'axios'
+var config = require('../../config')
+
+var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+
+var AXIOS = axios.create({
+  baseURL: backendUrl,
+  headers: { 'Access-Control-Allow-Origin': frontendUrl }
+})
+
 function PersonDto(name) {
   this.name = name
   this.events = []
@@ -17,23 +28,47 @@ export default {
       persons: [],
       newPerson: '',
       errorPerson: '',
-      response: []
+      response: [],
+      // event
+      events: [],
+      errorEvent: '',
     }
   },
+  // upon creation, do something
   created: function () {
-    // Test data
-    const p1 = new PersonDto('John')
-    const p2 = new PersonDto('Jill')
-    // Sample initial content
-    this.persons = [p1, p2]
+    // Initializing persons from backend
+    AXIOS.get('/persons')
+      .then(response => {
+        // JSON responses are automatically parsed.
+        this.persons = response.data
+      })
+      .catch(e => {
+        this.errorPerson = e
+      })
+    // Initializing events
+    AXIOS.get('/events')
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(e => {
+        this.errorEvent = e
+        // this.errors.push(e)
+      })
   },
   methods: {
     createPerson: function (personName) {
-      // Create a new person and add it to the list of people
-      var p = new PersonDto(personName)
-      this.persons.push(p)
-      // Reset the name field for new people
-      this.newPerson = ''
+      AXIOS.post('/persons/'.concat(personName), {}, {})
+        .then(response => {
+          // JSON responses are automatically parsed.
+          this.persons.push(response.data)
+          this.errorPerson = ''
+          this.newPerson = ''
+        })
+        .catch(e => {
+          var errorMsg = e.response.data.message
+          console.log(errorMsg)
+          this.errorPerson = errorMsg
+        })
     }
   }
 }
