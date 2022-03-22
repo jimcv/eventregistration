c3ce5df642323919747551206a3799758cd07d73
jimcv <71361453+jimcv@users.noreply.github.com>
2022-03-22 01:50:48 -0400

    Add state management in main.js

--- EventRegistration-Frontend/src/main.js
+++ EventRegistration-Frontend/src/main.js
@@ -6,9 +6,12 @@ import App from "./App";
 import router from "./router";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+// state management
+import Vuex from "vuex";
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+Vue.use(Vuex);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
@@ -16,5 +19,17 @@ new Vue({
   el: "#app",
   router,
   template: "<App/>",
-  components: { App },
+  components: { App }
+});
+
+// state management
+export const LOGINSTATE = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    }
+  }
 });
